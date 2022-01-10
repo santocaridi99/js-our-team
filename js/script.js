ebc74896e4a1398fd1923d27b0438c71bac92bb3
santocaridi99 <santocaridi99@gmail.com>
2022-01-10 15:49:53 +0100

    esercizio finito

---
+++ js/script.js
@@ -0,0 +1,67 @@
+//cambiato nella key image il valore di tutti aggiungendo /img sennò non funzionavano le img
+const team = [
+    {
+      name: 'Wayne Barnett',
+      role: 'Founder & CEO',
+      image: 'img/wayne-barnett-founder-ceo.jpg',
+    },
+    {
+      name: 'Angela Caroll',
+      role: 'Chief Editor',
+      image: 'img/angela-caroll-chief-editor.jpg',
+    },
+    {
+      name: 'Walter Gordon',
+      role: 'Office Manager',
+      image: 'img/walter-gordon-office-manager.jpg',
+    },
+    {
+      name: 'Angela Lopez',
+      role: 'Social Media Manager',
+      image: 'img/angela-lopez-social-media-manager.jpg',
+    },
+    {
+      name: 'Scott Estrada',
+      role: 'Developer',
+      image: 'img/scott-estrada-developer.jpg',
+    },
+    {
+      name: 'Barbara Ramos',
+      role: 'Graphic Designer',
+      image: 'img/barbara-ramos-graphic-designer.jpg',
+    },
+];
+//dichiaro variabile che si attacca al teamcontainer
+const teamContainer = document.querySelector('.team-container');
+//per ogni elemento del nostro array
+for(let i=0; i<team.length;i++){
+  //creo un div classe teamcard che appendo al container
+  const teamCard = document.createElement('div');
+  teamCard.className='team-card';
+  teamContainer.append(teamCard);
+  //creo un div classe cardImg che appendo al teamcard
+  const cardImage = document.createElement('div');
+  cardImage.className='card-image';
+  teamCard.append(cardImage);
+  // un tag img appeso al div cardImg
+  const img=document.createElement('img');
+  cardImage.append(img);
+  //un div cardText appeso al div class team card
+  const cardText = document.createElement('div');
+  cardText.className='card-text';
+  teamCard.append(cardText);
+  //ciclo per ogni key presente negli oggetti dell'array
+  for(let k in team[i]){
+    //cambio attributi al tag img 
+    img.setAttribute("alt",team[i].name);
+    //cambio src al tag img
+    img.src=team[i].image;
+    //inserisco nell'htm nel div cardtext i nomi e i ruoli delle card
+    cardText.innerHTML=`<h3>${team[i].name}</h3><p>${team[i].role}</p>`
+  }
+}
+
+
+
+
+  
